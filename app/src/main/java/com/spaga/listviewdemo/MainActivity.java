177989633b78d0727d410b5763ce873ba3c1b735
spagabtti <spagabtti@gmail.com>
2018-01-03 11:17:42 -0800

    edit

--- app/src/main/java/com/spaga/listviewdemo/MainActivity.java
+++ app/src/main/java/com/spaga/listviewdemo/MainActivity.java
@@ -24,5 +24,6 @@ public class MainActivity extends AppCompatActivity {
                 android.R.layout.simple_list_item_1, myNames);
 
         myListView.setAdapter(arrayAdapter);
+        // test
     }
 }
